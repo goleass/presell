dc06195a6fb7bf3853bfb7e70a29cab17424e2ee
Leonardo Assunção <leonardoassuncao@deltaglobal.com.br>
2024-10-21 11:03:55 -0300

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Helmet, HelmetProvider } from "react-helmet-async";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -25,9 +25,7 @@ const router = createBrowserRouter([
     element: (
       <HelmetProvider>
         <App>
-        <Helmet>
         <Pixel />
-        </Helmet>
         </App>
       </HelmetProvider>
     ),
